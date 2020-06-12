59e167a891cb220ea1f7d77ec0c42fa1ca32282c
bLeDy <deejaynof@gmail.com>
2020-06-12 23:02:44 +0200

    Big refactor to use PageObject pattern
    - Change webdriver to improve speed: `Firefox -> Chrome`
    - Change settings file name: `settings.json -> upload_settings.json`
    - Move credentials from `.env` to `upload_settings.json`
    - Remove `python-dotenv` requirement
    - Apply PageObject pattern
    - Remove `price` from settings until i manage to circunvent an obstructing lable
    - Bump wait time before and after file selection to 1s
    - Add changelog section to README

--- README.md
+++ README.md
@@ -7,10 +7,9 @@ the edges and neither well thought nor complete.
 
 ## User Requirements
 
-- Fill/replace values in [settings.json] before running the script
-- Create `.env` file with your lbry credentials (PASSWORD, EMAIL)
+- Fill [upload_settings.json] before running the script
 - Provide an absolute folder path
-- Have a thumbnail url available
+- Available thumbnail url
 - Song titles must match the format `{number} - {title}.{format}`
 - Folder structure as follows
 
@@ -40,18 +39,54 @@ the edges and neither well thought nor complete.
 > Files should be in a folder which is named after the album, which should be located
 > inside another folder that is named after the artist.
 
+## Usage
+
+- Create a virtual enviroment and activate it
+
+    ```sh
+    python3 -m virtualenv venv && source venv/bin/activate
+    ```
+
+- Install the requirements
+
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+- Fill `upload_settings.json`
+
+- Run the script
+
+    ```sh
+    python batch_upload.py
+    ```
+
+### Changelog
+
+- 2020-06-12:
+  - Change webdriver yo improve speed: `Firefox -> Chrome`
+  - Change settings file name: `settings.json -> upload_settings.json`
+  - Move credentials from `.env` to `upload_settings.json`
+  - Remove `python-dotenv` requirement
+  - Apply PageObject pattern
+  - Remove `price` from settings until i manage to circunvent an obstructing lable
+  - Bump wait time before and after file selection to 1s
+
 ### TODO
 
-- [ ] Better explanation of usage and capabilities
-- [ ] Add configuration guide to `README.md` with all valid values for `settings.json`
-- [ ] Add example credentials file or move them to `settings.json`
-- [ ] Validate `settings.json` before uploading any file
+- [x] Implement PageObject pattern
+- [x] Keep the upload page on sight when uploading the last file
+- [x] Better explanation of usage and capabilities _(not good enough)_
+- [ ] Add configuration guide to `README.md` with all valid values for `upload_settings.json`
+- [ ] Add example credentials file or move them to `upload_settings.json`
+- [ ] Validate `upload_settings.json` before uploading any file
 - [ ] Warn about naming issues before uploading any file (mainly punctuation)
-- [ ] Keep the upload page on sight when uploading the last file
 - [ ] Prepend artist, album and title info to description string
-- [ ] Better way of setting a description than a string in `settings.json`
+- [ ] Better way of setting a description than a string in `upload_settings.json`
 - [ ] Follow prefered selector order (`id > name > links text > css > xpath`)
-- [ ] PageObject pattern
+- [ ] Add argument to enable dark mode on the webpage
+- [ ] Prevent or react to "failed to fetch" error on login
+- [ ] Handle price option
 - [ ] Implement waits
 - [ ] Type hints
 - [ ] Unit tests
@@ -59,4 +94,4 @@ the edges and neither well thought nor complete.
 [lbry.tv]: https://lbry.tv/
 [selenium]: https://github.com/SeleniumHQ/selenium
 [pyautogui]: https://github.com/asweigart/pyautogui
-[settings.json]: settings.json
+[upload_settings.json]: upload_settings.json
--- batch_upload.py
+++ batch_upload.py
@@ -3,7 +3,6 @@ import time
 import json
 
 import pyautogui
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -11,186 +10,322 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
 
 
-def get_settings():
-    with open('settings.json', 'r') as fp:
-        settings = json.load(fp)
+class PageObject:
+    def __init__(self, driver):
+        self.driver = driver
 
-    return settings
+    def __banner(self):
+        return self.driver.find_element_by_css_selector('button.nag__button')
 
-
-def get_files(path, format):
-    songs = []
-    for file in os.listdir(settings['folder_path']):
-        if os.path.splitext(file)[-1] == settings['audio_format']:
-            if file not in settings['excluded']:
-                songs.append(file)
-
-    songs.sort(reverse=True)
-    return songs
+    def close_banners(self):
+        try:
+            self.__banner().click()
+            self.__banner().click()
+        except NoSuchElementException:
+            print('No banner found')
 
 
-def get_file_data(file):
-    path_head, album = os.path.split(settings['folder_path'])
-    _, artist = os.path.split(path_head)
-    title = f'{os.path.splitext(file)[0]} - {album} - {artist}'
+class LoginPage(PageObject):
+    def __email_field(self):
+        return self.driver.find_element_by_css_selector('#username')
 
-    # Clean name for claim url
-    sanitized_name = []
-    for char in os.path.splitext(file)[0]:
+    def __password_field(self):
+        return self.driver.find_element_by_css_selector('#password')
+
+    def __signin_button(self):
+        return self.driver.find_element_by_css_selector('[aria-label="Sign In"]')
+
+    def __continue_button(self):
+        return self.driver.find_element_by_css_selector('[aria-label="Continue"]')
+
+    def load_page(self):
+        self.driver.get('https://lbry.tv/$/signin')
+
+    def login(self, email, password):
+        self.__email_field().send_keys(email)
+        self.__signin_button().click()
+        time.sleep(0.5)
+        self.__password_field().send_keys(password)
+        self.__continue_button().click()
+
+
+# def close_success_popup():
+#     # FIXME: waiting for fill_and_publish to be separated
+#     try:
+#         driver.find_element_by_css_selector('[aria-label="Close"]').click()
+#     except NoSuchElementException:
+#         print('No success popup found')
+
+
+# def detect_popup(func):
+#     def func_wrapper(*args):
+#         try:
+#             func(*args)
+#         except ElementClickInterceptedException:
+#             driver.find_element_by_css_selector('[aria-label="Close"]').click()
+#             func(*args)
+#     return func_wrapper
+
+
+# def detect_popup(driver):
+#     def popup_decorator(func):
+#         def func_wrapper(*args):
+#             try:
+#                 func(*args)
+#             except ElementClickInterceptedException:
+#                 print('Popup detected, trying to close it and continue')
+#                 driver.find_element_by_css_selector('[aria-label="Close"]').click()
+#                 func(*args)
+#         return func_wrapper
+#     return popup_decorator
+
+
+class UploadPage(PageObject):
+    def __init__(self, driver, settings):
+        self.settings = settings
+        super().__init__(driver)
+
+    def __file_button(self):
+        return self.driver.find_element_by_css_selector('[aria-label="Choose File"]')
+
+    def __title(self):
+        return self.driver.find_element_by_css_selector('#content_title')
+
+    def __description(self):
+        return self.driver.find_element_by_css_selector('#content_description')
+
+    def __thumbnail_button(self):
+        return self.driver.find_element_by_css_selector(
+            '[aria-label="Enter a thumbnail URL"]'
+        )
+
+    def __thumbnail(self):
+        return self.driver.find_element_by_css_selector('#content_thumbnail')
+
+    def __tags(self):
+        return self.driver.find_element_by_css_selector('.tag__input')
+
+    def __channel_list(self):
+        return self.driver.find_element_by_css_selector('#channel')
+
+    def __claim_url(self):
+        return self.driver.find_element_by_css_selector('#content_name')
+
+    def __deposit(self):
+        return self.driver.find_element_by_css_selector('#content_bid')
+
+    def __price_button(self):
+        return self.driver.find_element_by_css_selector('#content_cost')
+
+    def __price(self):
+        return self.driver.find_element_by_css_selector('#content_cost_amount_amount')
+
+    def __options_button(self):
+        return self.driver.find_element_by_css_selector(
+            '[aria-label="Additional Options"]'
+        )
+
+    def __language_list(self):
+        return self.driver.find_element_by_css_selector('#content_language')
+
+    def __license_list(self):
+        return self.driver.find_element_by_css_selector('#license')
+
+    def __copyright_notice(self):
+        return self.driver.find_element_by_css_selector('#copyright-notice')
+
+    def __publish_button(self):
+        return self.driver.find_element_by_css_selector('[aria-label="Publish"]')
+
+    def __publish_next_button(self):  # FIXME: replace with load_page
+        return self.driver.find_element_by_link_text('Publish')
+
+    def load_page(self):
+        self.driver.get('https://lbry.tv/$/publish')
+
+    def choose_file(self, song_name):
+        self.__file_button().click()
+
+        time.sleep(1)
+        file_path = f'{self.settings["folder_path"]}/{song_name}'
+        for folder in file_path.lstrip(os.sep).split(os.sep):
+            pyautogui.hotkey('divide')
+            pyautogui.write(folder)
+
+        time.sleep(0.5)  # FIXME: slow computers might need more time
+        pyautogui.press('enter')
+        time.sleep(1)
+
+    # @detect_popup
+    def fill_title(self, upload_title):
+        self.__title().send_keys(upload_title)
+
+    # @detect_popup
+    def fill_description(self):
+        self.__description().send_keys(self.settings['description'])
+
+    # @detect_popup
+    def fill_thumbnail_url(self):
+        self.__thumbnail_button().click()
+        self.__thumbnail().send_keys(self.settings['thumbnail_url'])
+
+    # @detect_popup
+    def fill_tags(self):
+        self.__tags().send_keys(','.join(self.settings['tags']), Keys.ENTER)
+
+    # @detect_popup
+    def select_channel(self):
+        Select(self.__channel_list()).select_by_value(self.settings['channel'])
+
+    # @detect_popup
+    def fill_claim_name(self, claim_name):
+        claim = self.__claim_url()
+        claim.clear()
+        claim.send_keys(claim_name)
+
+    # @detect_popup
+    def fill_deposit(self):
+        content_bid = self.__deposit()
+        content_bid.clear()
+        content_bid.send_keys(self.settings['deposit'])
+
+    # @detect_popup
+    def fill_price(self):
+        self.__price_button().click()
+        price = self.__price()
+        price.clear()
+        price.send_keys(self.settings['price'])
+
+    # @detect_popup
+    def open_additional_options(self):
+        self.__options_button().click()
+
+    # @detect_popup
+    def select_language(self):
+        Select(self.__language_list()).select_by_value(self.settings['language'])
+
+    # @detect_popup
+    def select_license(self):
+        Select(self.__license_list()).select_by_value(self.settings['license']['type'])
+
+    # @detect_popup
+    def fill_license(self):
+        copyright_notice = self.__copyright_notice()
+        copyright_notice.clear()
+        copyright_notice.send_keys(self.settings['license']['notice'])
+
+    # @detect_popup
+    def publish(self):
+        self.__publish_button().click()
+
+    def upload_song(self, song_data, first_song=False):
+        self.choose_file(song_data['song_name'])
+        self.fill_title(song_data['upload_title'])
+        self.fill_description()
+        self.fill_thumbnail_url()
+        self.fill_tags()
+        self.select_channel()
+        self.fill_claim_name(song_data['claim_name'])
+
+        if self.settings['deposit'] is not None:
+            self.fill_deposit()
+
+        # if self.settings['price'] is not None:  # FIXME: label is obstructing so can't click
+        #     self.fill_price()
+
+        if first_song:
+            self.open_additional_options()
+
+        self.select_language()
+
+        if self.settings['license']['type'] is not None:
+            self.select_license()
+            self.fill_license()  # TODO: only fill if license == "copyright"
+
+        self.publish()
+
+    def continue_publishing(self):
+        self.__publish_next_button().click()
+
+
+def get_upload_settings():
+    with open('upload_settings.json', 'r') as fp:
+        return json.load(fp)
+
+
+def get_song_names(folder_path, audio_format, excluded):
+    song_names = []
+    for file in os.listdir(folder_path):
+        if os.path.splitext(file)[-1] == audio_format:
+            if file not in excluded:
+                song_names.append(file)
+
+    return song_names
+
+
+def get_sanitized_claim_name(song_name):
+    sanitized_name = ''
+    for char in os.path.splitext(song_name)[0]:
         if char.isalpha():
-            sanitized_name.append(char)
+            sanitized_name += char
         elif char == ' ':
             if sanitized_name and sanitized_name[-1] != char:
-                sanitized_name.append(char)
+                sanitized_name += char
 
-    claim_name = ''.join(sanitized_name).lower().split(' - ', 1)[-1]
-    claim_name_url = os.path.splitext(claim_name)[0].replace(' ', '-')
+    return sanitized_name.lower().split(' - ', 1)[-1]
 
-    song_data = {
+
+def get_song_data(song_name, folder_path):
+    path_head, album = os.path.split(folder_path)
+    _, artist = os.path.split(path_head)
+    upload_title = f'{os.path.splitext(song_name)[0]} - {album} - {artist}'
+    sanitized_claim_name = get_sanitized_claim_name(song_name)
+    claim_name = os.path.splitext(sanitized_claim_name)[0].replace(' ', '-')
+
+    return {
         'album': album,
         'artist': artist,
-        'file': file,
-        'title': title,
-        'claim_name_url': claim_name_url,
+        'song_name': song_name,
+        'upload_title': upload_title,
+        'claim_name': claim_name,
     }
-    return song_data
-
-
-def launch_webdriver():
-    driver = webdriver.Firefox()
-    driver.implicitly_wait(10)
-    return driver
-
-
-def get_rid_of_bottom_banners(driver):  # NOTE: usually there are only two
-    try:
-        driver.find_element_by_css_selector('button.nag__button').click()
-        time.sleep(0.5)
-        driver.find_element_by_css_selector('button.nag__button').click()
-    except NoSuchElementException:
-        pass
-
-
-def get_rid_of_success_popup(driver):
-    try:
-        driver.find_element_by_css_selector('[aria-label="Close"]').click()
-    except NoSuchElementException:
-        pass
-
-
-def sign_in(driver):
-    driver.find_element_by_css_selector('#username').send_keys(os.getenv('EMAIL'))
-    driver.find_element_by_css_selector('[aria-label="Sign In"]').click()
-    time.sleep(0.5)
-    driver.find_element_by_css_selector('#password').send_keys(os.getenv('PASSWORD'))
-    driver.find_element_by_css_selector('[aria-label="Continue"]').click()
-
-
-def fill_and_publish(driver, settings, song_data, first_upload):
-    # Choose file
-    driver.find_element_by_css_selector('[aria-label="Choose File"]').click()
-
-    file_path = f'{settings["folder_path"]}/{song_data["file"]}'
-    for folder in file_path.lstrip(os.sep).split(os.sep):
-        pyautogui.hotkey('divide')
-        pyautogui.write(folder)
-
-    time.sleep(0.5)
-    pyautogui.press('enter')
-    time.sleep(0.5)
-
-    # Fill title and description
-    driver.find_element_by_css_selector('#content_title').send_keys(song_data['title'])
-    driver.find_element_by_css_selector('#content_description').send_keys(
-        settings['description']
-    )
-
-    # Fill thumbnail url
-    driver.find_element_by_css_selector('[aria-label="Enter a thumbnail URL"]').click()
-    driver.find_element_by_css_selector('#content_thumbnail').send_keys(
-        settings['thumbnail_url']
-    )
-
-    # Enter tags
-    driver.find_element_by_css_selector('.tag__input').send_keys(
-        ', '.join(settings['tags']), Keys.ENTER
-    )
-
-    # Select channel
-    Select(driver.find_element_by_css_selector('#channel')).select_by_value(
-        settings['channel']
-    )
-
-    # Set url and deposit
-    content_name = driver.find_element_by_css_selector('#content_name')
-    content_name.clear()
-    content_name.send_keys(song_data['claim_name_url'])
-
-    deposit = settings['deposit']
-    if deposit is not None:
-        content_bid = driver.find_element_by_css_selector('#content_bid')
-        content_bid.clear()
-        content_bid.send_keys(deposit)
-
-    # # Set price  # FIXME: label is obstructing so can't click
-    # amount = settings['price']
-    # if amount is not None:
-    #     driver.find_element_by_css_selector('#content_cost').click()
-    #     content_cost = driver.find_element_by_css_selector(
-    #         '#content_cost_amount_amount'
-    #     )
-    #     content_cost.clear()
-    #     content_cost.send_keys(amount)
-
-    # Select language and license
-    if first_upload:
-        driver.find_element_by_css_selector('[aria-label="Additional Options"]').click()
-
-    Select(driver.find_element_by_css_selector('#content_language')).select_by_value(
-        settings['language']
-    )
-
-    if settings['license']['type'] is not None:
-        Select(driver.find_element_by_css_selector('#license')).select_by_value(
-            settings['license']['type']
-        )
-        copyright_notice = driver.find_element_by_css_selector('#copyright-notice')
-        copyright_notice.clear()
-        copyright_notice.send_keys(settings['license']['notice'])
-
-    # Publish
-    driver.find_element_by_css_selector('[aria-label="Publish"]').click()
-
-
-def upload_song(driver, song, first_upload):
-    song_data = get_file_data(song)
-    fill_and_publish(driver, settings, song_data, first_upload)
-    next_publish(driver, song)
-
-
-def next_publish(driver, last_song):
-    print('[UPLOADED]', last_song)
-    driver.find_element_by_link_text('Publish').click()
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    driver = launch_webdriver()
-    driver.get('https://lbry.tv/$/signin')
+    upload_settings = get_upload_settings()
 
-    sign_in(driver)
-    get_rid_of_bottom_banners(driver)
-    driver.get('https://lbry.tv/$/publish')
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
 
-    settings = get_settings()
-    songs = get_files(settings['folder_path'], settings['audio_format'])
+    login_page = LoginPage(driver)
+    login_page.load_page()
+    login_page.login(upload_settings['email'], upload_settings['password'])
+    login_page.close_banners()
 
-    first_upload = True
-    for song in songs:
-        try:
-            upload_song(driver, song, first_upload)
-        except ElementClickInterceptedException:
-            get_rid_of_success_popup(driver)
-            upload_song(driver, song, first_upload)
+    upload_page = UploadPage(driver, upload_settings)
+    upload_page.load_page()
 
-        first_upload = False
+    songs = get_song_names(
+        upload_settings['folder_path'],
+        upload_settings['audio_format'],
+        upload_settings['excluded'],
+    )
+    song_count = len(songs)
+
+    while len(songs) > 0:
+        # From last to first so they are ordered on the feed
+        song_name = songs.pop()
+        song_data = get_song_data(song_name, upload_settings['folder_path'])
+
+        # Click "Additional Options" only on the first publish
+        if len(songs) + 1 == song_count:
+            upload_page.upload_song(song_data, first_song=True)
+        else:
+            upload_page.upload_song(song_data)
+
+        print('[PUBLISHED]', song_name)
+        time.sleep(0.5)
+
+        # Keep the upload page on sight when uploading the last file
+        if not len(songs) == 0:
+            upload_page.continue_publishing()
