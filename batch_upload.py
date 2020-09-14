930188a374f25c64639daebc68f00608c8fc98f6
bLeDy <deejaynof@gmail.com>
2020-09-14 19:37:16 +0200

    Use semi-private methods

--- batch_upload.py
+++ batch_upload.py
@@ -35,13 +35,13 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    def __banner(self):
+    def _banner(self):
         return self.driver.find_element_by_css_selector('button.nag__button')
 
     def close_banners(self):
         try:
-            self.__banner().click()
-            self.__banner().click()
+            self._banner().click()
+            self._banner().click()
         except NoSuchElementException:
             pass
 
@@ -50,27 +50,27 @@ class BasePage:
 
 
 class LoginPage(BasePage):
-    def __email_field(self):
+    def _email_field(self):
         return self.driver.find_element_by_css_selector('#username')
 
-    def __password_field(self):
+    def _password_field(self):
         return self.driver.find_element_by_css_selector('#password')
 
-    def __signin_button(self):
+    def _signin_button(self):
         return self.driver.find_element_by_css_selector('[aria-label="Sign In"]')
 
-    def __continue_button(self):
+    def _continue_button(self):
         return self.driver.find_element_by_css_selector('[aria-label="Continue"]')
 
     def load_page(self):
         self.driver.get('https://lbry.tv/$/signin')
 
     def login(self):
-        self.__email_field().send_keys(EMAIL)
-        self.__signin_button().click()
+        self._email_field().send_keys(EMAIL)
+        self._signin_button().click()
         time.sleep(0.5)
-        self.__password_field().send_keys(PASSWORD)
-        self.__continue_button().click()
+        self._password_field().send_keys(PASSWORD)
+        self._continue_button().click()
 
 
 def close_success_popup(func):
@@ -86,66 +86,66 @@ def close_success_popup(func):
 
 
 class UploadPage(BasePage):
-    def __file_button(self):
+    def _file_button(self):
         return self.driver.find_element_by_css_selector('[aria-label="Browse"]')
 
-    def __title(self):
+    def _title(self):
         return self.driver.find_element_by_css_selector('#content_title')
 
-    def __description(self):
+    def _description(self):
         return self.driver.find_element_by_css_selector('#content_description')
 
-    def __thumbnail_button(self):
+    def _thumbnail_button(self):
         return self.driver.find_element_by_css_selector(
             '[aria-label="Enter a thumbnail URL"]'
         )
 
-    def __thumbnail(self):
+    def _thumbnail(self):
         return self.driver.find_element_by_css_selector('#content_thumbnail')
 
-    def __tags(self):
+    def _tags(self):
         return self.driver.find_element_by_css_selector('.tag__input')
 
-    def __channel_list(self):
+    def _channel_list(self):
         return self.driver.find_element_by_css_selector('#ID_FF_SELECT_CHANNEL')
 
-    def __claim_url(self):
+    def _claim_url(self):
         return self.driver.find_element_by_css_selector('#content_name')
 
-    def __deposit(self):
+    def _deposit(self):
         return self.driver.find_element_by_css_selector('#content_bid')
 
-    def __price_button(self):
+    def _price_button(self):
         return self.driver.find_element_by_css_selector('label[for="content_cost"]')
 
-    def __price(self):
+    def _price(self):
         return self.driver.find_element_by_css_selector('#content_cost_amount_amount')
 
-    def __options_button(self):
+    def _options_button(self):
         return self.driver.find_element_by_css_selector(
             '[aria-label="Additional Options"]'
         )
 
-    def __language_list(self):
+    def _language_list(self):
         return self.driver.find_element_by_css_selector('#content_language')
 
-    def __license_list(self):
+    def _license_list(self):
         return self.driver.find_element_by_css_selector('#license')
 
-    def __copyright_notice(self):
+    def _copyright_notice(self):
         return self.driver.find_element_by_css_selector('#copyright-notice')
 
-    def __publish_button(self):
+    def _publish_button(self):
         return self.driver.find_element_by_css_selector('[aria-label="Upload"]')
 
-    def __publish_next_button(self):
+    def _publish_next_button(self):
         return self.driver.find_element_by_css_selector('a[href="/$/upload"]')
 
     def load_page(self):
         self.driver.get('https://lbry.tv/$/publish')
 
     def choose_file(self, song_name):  # NOTE: designed for nautilus
-        self.__file_button().click()
+        self._file_button().click()
         pyperclip.copy(os.path.join(FOLDER_PATH, song_name))
         time.sleep(1)
         pyautogui.hotkey('divide')
@@ -156,61 +156,61 @@ class UploadPage(BasePage):
         time.sleep(1)
 
     def fill_title(self, upload_title):
-        self.__title().send_keys(upload_title)
+        self._title().send_keys(upload_title)
 
     def fill_description(self):
-        self.__description().send_keys(DESCRIPTION)
+        self._description().send_keys(DESCRIPTION)
 
     def select_thumbnail_url(self):
-        self.__thumbnail_button().click()
+        self._thumbnail_button().click()
 
     def fill_thumbnail_url(self):
-        self.__thumbnail().send_keys(THUMBNAIL_URL)
+        self._thumbnail().send_keys(THUMBNAIL_URL)
 
     def fill_tags(self):
-        self.__tags().send_keys(','.join(TAGS), Keys.ENTER)
+        self._tags().send_keys(','.join(TAGS), Keys.ENTER)
 
     def select_channel(self):
-        Select(self.__channel_list()).select_by_value(CHANNEL)
+        Select(self._channel_list()).select_by_value(CHANNEL)
 
     def fill_claim_name(self, claim_name):
-        claim = self.__claim_url()
+        claim = self._claim_url()
         claim.clear()
         claim.send_keys(claim_name)
 
     def fill_deposit(self):
-        content_bid = self.__deposit()
+        content_bid = self._deposit()
         content_bid.clear()
         content_bid.send_keys(str(DEPOSIT))
 
     def select_price(self):
-        self.__price_button().click()
+        self._price_button().click()
 
     def fill_price(self):
-        price = self.__price()
+        price = self._price()
         price.clear()
         price.send_keys(str(PRICE))
 
     def open_additional_options(self):
-        self.__options_button().click()
+        self._options_button().click()
 
     def select_language(self):
-        Select(self.__language_list()).select_by_value(LANGUAGE)
+        Select(self._language_list()).select_by_value(LANGUAGE)
 
     def select_license(self):
-        Select(self.__license_list()).select_by_value(LICENSE_TYPE)
+        Select(self._license_list()).select_by_value(LICENSE_TYPE)
 
     def fill_license(self):
-        copyright_notice = self.__copyright_notice()
+        copyright_notice = self._copyright_notice()
         copyright_notice.clear()
         copyright_notice.send_keys(LICENSE_NOTICE)
 
     def publish(self):
-        self.__publish_button().click()
+        self._publish_button().click()
 
     @close_success_popup
     def continue_publishing(self):
-        self.__publish_next_button().click()
+        self._publish_next_button().click()
 
     @close_success_popup
     def upload_song(self, song_data, first_song):
